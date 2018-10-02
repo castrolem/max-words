dfcfe4581ced7b6d50f1d5400eace5340aefbd87
Luis Castro <luiscastroem@gmail.com>
2018-10-02 19:52:15 +0200

    feat: Add themes

--- src/Constants/colors.js
+++ src/Constants/colors.js
@@ -1,10 +1,42 @@
-const WHITE = ['#FFFFFF', '#000000'];
-const ORANGE = ['#F5A723', '#5B3D05'];
-const RED = ['#A80115', '#FFFFFF'];
-const BLUE = ['#2994C6', '#09415B'];
-const GREEN = ['#417506', '#BAEE88'];
-const BROWN = ['#452B14', '#CF9B6B'];
-const YELLOW = ['#272727', '#F9C727'];
-const GREY = ['#4A4A4A', '#9C9C9C'];
-const BLACK = ['#000000', '#FFFFFF'];
-const PURPLE = ['#25055B', '#8E4CEF'];
+const THEMES = {
+  'White Stripes': {
+    textColor: '#FFFFFF',
+    backgroundColor: '#000000',
+  },
+  'Smashing Pumpkins': {
+    textColor: '#5C3C07',
+    backgroundColor: '#F5A623',
+  },
+  'Red Hot Chilli Peppers': {
+    textColor: '#FFFFFF',
+    backgroundColor: '#A80015',
+  },
+  'Blue Oyster Cult': {
+    textColor: '#0E3E53',
+    backgroundColor: '#2895C6',
+  },
+  'Green Day': {
+    textColor: '#417505',
+    backgroundColor: '#B8E986',
+  },
+  'Zac Brown Band': {
+    textColor: '#452B14',
+    backgroundColor: '#D19A6A',
+  },
+  'The Chemical Brothers': {
+    textColor: '#F8C41C',
+    backgroundColor: '#272727',
+  },
+  'Fantastic Negrito': {
+    textColor: '#FFFFFF',
+    backgroundColor: '#000000',
+  },
+  Metallica: {
+    textColor: '#AFAFAF',
+    backgroundColor: '#323232',
+  },
+  'Deep Purple': {
+    textColor: '#8D6CEF',
+    backgroundColor: '#25045C',
+  },
+};
